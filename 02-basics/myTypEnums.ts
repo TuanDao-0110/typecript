a8147a8be39524454dec8e6506a03d8b2e79a1ed
tuan <rickytuan1991@gmail.com>
2022-12-25 23:25:40 +0200

    add new enums

---
+++ 02-basics/myTypEnums.ts
@@ -0,0 +1,9 @@
+// const user: (string | number)[] = ["hc"];
+let tUser: [string, number, boolean];
+tUser = ["hc", 13, true];
+// define
+let rgb: [number, number, number] = [224, 335, 112];
+// 3. define as type : 
+type User = [number,string]
+let newUser:User=[112,'id@gmail.com']
+export {};
