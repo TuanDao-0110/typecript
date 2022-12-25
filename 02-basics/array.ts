39c7ea6d7a833f23469139b339acaab7200f18c7
tuan <rickytuan1991@gmail.com>
2022-12-25 15:31:31 +0200

    addd new array type

--- 02-basics/array.ts
+++ 02-basics/array.ts
@@ -1,3 +1,24 @@
+// 1. define as type
 const superHero: string[] = [];
-superHero.push('hello')
-console.log(superHero)
+
+// const heroPower : number []= []
+// heroPower.push(3)
+// 2.define other synctag
+const heroPower: Array<number> = [];
+// 3. define by type
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUser: User[] = [];
+allUser.push({
+  name: "tuan",
+  isActive: true,
+});
+// 4. define by array of type array number
+const Mlmodels: number[][] = [];
+Mlmodels.push([32, 324]);
+// 
+superHero.push("hello");
+console.log(superHero);
