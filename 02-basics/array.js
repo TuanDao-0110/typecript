5d02c369ea85964451c1ae223514279c8c777ae9
tuan <rickytuan1991@gmail.com>
2022-12-25 16:15:07 +0200

    add new array type

--- 02-basics/array.js
+++ 02-basics/array.js
@@ -1,3 +1,34 @@
+"use strict";
+exports.__esModule = true;
+// 1. define as type
 var superHero = [];
-superHero.push('hello');
-console.log(superHero);
+// const heroPower : number []= []
+// heroPower.push(3)
+// 2.define other synctag
+var heroPower = [];
+var allUser = [];
+allUser.push({
+    name: "tuan",
+    isActive: true
+});
+// 4. define by array of type array number
+var Mlmodels = [];
+Mlmodels.push([32, 324]);
+superHero.push("hello");
+//5. readonly array :
+var schoolName = ["a", "b", "c", 4];
+// 6. function array :
+var addSchoolName = function (newSchool) { };
+// we can write
+// let newStudent: Array<student> = [];
+// or
+var newStudent = [];
+// newStudent.push([4, "tuan", true]);
+// newStudent.push([10, "long", false]);
+newStudent.push([3, "tuan", true]);
+// 8. define as fn's parameter
+var addNewStudent = function (morestudent) {
+    console.log(morestudent);
+    return newStudent;
+};
+console.log(newStudent);
--- 02-basics/array.ts
+++ 02-basics/array.ts
@@ -1,24 +1,42 @@
 // 1. define as type
-const superHero: string[] = [];
+let superHero: string[] = [];
 
 // const heroPower : number []= []
 // heroPower.push(3)
 // 2.define other synctag
-const heroPower: Array<number> = [];
+let heroPower: Array<number> = [];
 // 3. define by type
 type User = {
   name: string;
   isActive: boolean;
 };
 
-const allUser: User[] = [];
+let allUser: User[] = [];
 allUser.push({
   name: "tuan",
   isActive: true,
 });
 // 4. define by array of type array number
-const Mlmodels: number[][] = [];
+let Mlmodels: number[][] = [];
 Mlmodels.push([32, 324]);
-// 
 superHero.push("hello");
-console.log(superHero);
+//5. readonly array :
+let schoolName: ReadonlyArray<string | number> = ["a", "b", "c", 4];
+// 6. function array :
+let addSchoolName = (newSchool: Array<string>): void => {};
+// 7. define as array type
+type student = [age: number, name: string, isActived: boolean];
+// we can write
+// let newStudent: Array<student> = [];
+// or
+let newStudent: student[] = [];
+newStudent.push([4, "tuan", true]);
+newStudent.push([10, "long", false]);
+// 8. define as fn's parameter
+const addNewStudent = (morestudent: student) => {
+  console.log(morestudent);
+  return newStudent;
+};
+
+console.log(newStudent);
+export {};
