3e9b2adc066bf2063b6caaf83d528e06f9c3b46f
tuan <rickytuan1991@gmail.com>
2022-12-26 19:03:56 +0200

    interface

--- 02-basics/interface.ts
+++ 02-basics/interface.ts
@@ -1,5 +1,33 @@
 interface User {
+  readonly dbId: number;
   email: string;
   userId: number;
+  googleId?: string;
+  //   startTrail?: () => string;
+  //   or we can write
+  startTrail(): string;
+  getCoupond(couponname: string, value: number): number;
 }
-const newUser: User = { email: "h@h.co", userId: 221 };
+
+interface User {
+  githubToken: string;
+}
+interface User {
+    
+}
+// 1. apply to new variable
+
+const newUser: User = {
+  dbId: 3,
+  email: "h@h.co",
+  userId: 4,
+  startTrail: () => {
+    return "return as string";
+  },
+  getCoupond(couponname: "new couponname", value: 4) {
+    return 10;
+  },
+  githubToken: "github",
+};
+newUser.email = "tuna";
+// newUser.dbId=4 ==> can not change due to read only
