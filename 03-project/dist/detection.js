05989a4d40ebcdac0981c7e970e1259599bd2851
tuan <rickytuan1991@gmail.com>
2022-12-27 22:20:26 +0200

    add discDiscriminated" g

--- 03-project/dist/detection.js
+++ 03-project/dist/detection.js
@@ -22,3 +22,33 @@ const user1 = {
 };
 // checking it:
 checkAdmin(user1);
+// pet is Fish ==> help to return a in case it is Fish, Fn have to return a type of Fish.
+const isFish = (pet) => {
+    return pet.swim !== undefined;
+};
+const getFood = (pet) => {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    return "bird food";
+};
+const getTrueShape = (shape) => {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    //   return shape.side * shape.side;
+};
+const getArea = (shape) => {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+};
--- 03-project/src/detection.ts
+++ 03-project/src/detection.ts
@@ -10,7 +10,7 @@ const provided = (id: string | null | number) => {
   console.log("provide id ");
   return;
 };
-// checking solution:
+//1. IN checking solution:
 interface User {
   name: string;
   email: string;
@@ -33,3 +33,59 @@ const user1: User = {
 };
 // checking it:
 checkAdmin(user1);
+// 2. instanceof
+// 3. type:
+type Fish = {
+  swim(): void;
+};
+type Bird = {
+  fly(): void;
+};
+// pet is Fish ==> help to return a in case it is Fish, Fn have to return a type of Fish.
+const isFish = (pet: Fish | Bird): pet is Fish => {
+  return (pet as Fish).swim !== undefined;
+};
+
+const getFood = (pet: Fish | Bird) => {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  }
+  return "bird food";
+};
+
+// 1. what is Discriminated
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+interface Square {
+  kind: "square";
+  side: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+type Shape = Circle | Square | Rectangle;
+const getTrueShape = (shape: Shape) => {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  //   return shape.side * shape.side;
+};
+
+const getArea = (shape: Shape) => {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default:
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
+};
