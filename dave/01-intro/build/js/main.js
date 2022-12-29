f1c85755c92f4e641d62ef1e71c365c0780c82fd
tuan <rickytuan1991@gmail.com>
2022-12-30 01:52:49 +0200

    add new index signature vs key vs record utility type

--- dave/01-intro/build/js/main.js
+++ dave/01-intro/build/js/main.js
@@ -1,29 +1,44 @@
 "use strict";
-class Guitarist {
-    constructor(name, instrument) {
-        this.name = name;
-        this.instrument = instrument;
-    }
-    play(action) {
-        return "";
-    }
+const todayTransaction = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+const pizza = "Pizza";
+const newOrder = [];
+newOrder.push({ data: 4 }, { data1: 5, data3: 5 });
+newOrder[0] = { data4: 5 };
+console.log(newOrder);
+console.log(todayTransaction[pizza]);
+const student1 = {
+    name: "tuan",
+    GPA: 4,
+};
+const student2 = {
+    name: "tuan",
+    GPA: 4,
+    classes: [4, 5],
+};
+for (const key in student1) {
+    //   console.log(`${key} : ${student1[key as keyof Student]}`);
 }
-class Peeps {
-    constructor(other = Peeps.getCount()) {
-        this.other = other;
-    }
-    static getCount() {
-        return Peeps.count + 50;
-    }
-    getOther() {
-        console.log(this.other);
-    }
-}
-Peeps.count = 1;
-const newPeeps = new Peeps();
-// console.log(newPeeps.getCount());
-newPeeps.getOther();
-class other extends Peeps {
-}
-const a = new other();
-// console.log(a.getCount());
+Object.keys(student1).map((key) => {
+    // this return as key of this student 1 type
+    //  --> TS will know that key in this case is Key of student1
+    console.log(student1[key]);
+});
+//3.  combime to function
+const logStudentKey = (student, 
+// we can write like this:
+key
+// or we can write: --> it will back straight to parent type to define the key
+//   key: keyof Student
+) => {
+    console.log(`student key ${key}: ${student[key]}`);
+};
+logStudentKey(student1, "GPA");
+let incomes = {
+    bonus: 4,
+    salary: 4,
+    sidehustle: '4',
+};
--- dave/01-intro/src/main.ts
+++ dave/01-intro/src/main.ts
@@ -1,29 +1,87 @@
-interface Musician {
+// // index signatures:
+// interface Transaction {
+//   Pizza: number;
+//   Books: number;
+//   Job: number;
+// }
+//1. introduction of index signature
+interface Transaction {
+  // we define all index inside object interface is string vs value is number
+  //   all the variable will be string with return type is number
+  readonly [index: string]: number;
+  //   Pizza: number;
+  //   Books: number;
+  //   Job: number;
+}
+const todayTransaction: Transaction = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+};
+const pizza: string = "Pizza";
+
+const newOrder: Transaction[] = [];
+newOrder.push({ data: 4 }, { data1: 5, data3: 5 });
+newOrder[0] = { data4: 5 };
+console.log(newOrder);
+console.log(todayTransaction[pizza]);
+
+//2. without index signature
+
+interface Student {
+  //   [key: string]: string | number | number[] | undefined;
   name: string;
-  instrument: string;
-  play(action: string): string;
+  GPA: number;
+  //   we use undefined becuase may be we dont create it in object --> that why value maybe underfined
+  classes?: number[];
 }
 
-class Guitarist implements Musician {
-  constructor(public name: string, public instrument: string) {}
-  play(action: string): string {
-    return "";
-  }
-}
-class Peeps {
-  static count: number = 1;
-  constructor(public other: number = Peeps.getCount()) {}
-  static getCount(): number {
-    return Peeps.count + 50;
-  }
-  getOther() {
-    console.log(this.other);
-  }
-}
-const newPeeps = new Peeps();
-// console.log(newPeeps.getCount());
-newPeeps.getOther();
-class other extends Peeps {}
+const student1: Student = {
+  name: "tuan",
+  GPA: 4,
+};
 
-const a = new other();
-// console.log(a.getCount());
+const student2: Student = {
+  name: "tuan",
+  GPA: 4,
+  classes: [4, 5],
+};
+
+for (const key in student1) {
+  //   console.log(`${key} : ${student1[key as keyof Student]}`);
+}
+
+Object.keys(student1).map((key) => {
+  // this return as key of this student 1 type
+  //  --> TS will know that key in this case is Key of student1
+  console.log(student1[key as keyof typeof student1]);
+});
+
+//3.  combime to function
+const logStudentKey = (
+  student: Student,
+  // we can write like this:
+  key: keyof typeof student
+  // or we can write: --> it will back straight to parent type to define the key
+  //   key: keyof Student
+): void => {
+  console.log(`student key ${key}: ${student[key]}`);
+};
+
+logStudentKey(student1, "GPA");
+
+// 4.
+
+// interface Incomes {
+//   [key: string]: number;
+// }
+
+type Stream = "salary" | "bonus" | "sidehustle";
+// Record will be use to store all key, and return value type
+type Incomes = Record<Stream, number|string>;
+
+let incomes: Incomes = {
+  bonus:4,
+  salary: 4,
+  sidehustle: '4',
+};
