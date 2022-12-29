193cd84f28dc74b6dde8a7b7ed26a7c4c9a7446b
tuan <rickytuan1991@gmail.com>
2022-12-29 14:13:33 +0200

    enums

--- dave/01-intro/build/js/main.js
+++ dave/01-intro/build/js/main.js
@@ -3,3 +3,12 @@ let guitar1 = {
     name: "player",
     active: true,
 };
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 10] = "U";
+    Grade["D"] = "1";
+    Grade["C"] = "time";
+    Grade["B"] = "not";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade["U"]);
--- dave/01-intro/src/main.ts
+++ dave/01-intro/src/main.ts
@@ -1,8 +1,17 @@
 type Guitarist = {
   name: string;
   active: boolean;
+  player?: (number | string)[];
 };
 let guitar1: Guitarist = {
   name: "player",
   active: true,
 };
+enum Grade {
+  U = 10,
+  D = "1",
+  C = "time",
+  B = "not",
+  A = 5,
+}
+console.log(Grade["U"]);
