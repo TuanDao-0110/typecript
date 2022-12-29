5238cd08b38a987685ec7b9a4618b77dddab5163
tuan <rickytuan1991@gmail.com>
2022-12-29 23:36:40 +0200

    add class

--- dave/01-intro/build/js/main.js
+++ dave/01-intro/build/js/main.js
@@ -1,38 +1,29 @@
 "use strict";
-let a = "a";
-let b = a; // less specific
-let c = a; // more specific
-let d = "world";
-let e = "world";
-const addOrConcat = (a, b, c) => {
-    if (c === "add") {
-        return a + b;
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
     }
-    return "" + a + b;
-};
-// we know that addOrConcat will return as string .
-// ==> so we use  'as string' to specify type of fn.
-// ==> so we will assume that fn will return only string event it can be number|string.
-let myVal = addOrConcat(2, 2, "concat");
-//1. be careful that fn may return a string:
-let nexVal = addOrConcat(2, 2, "concat");
-console.log(myVal);
-//2. the DOM :
-const img = document.querySelector("img");
-//  but we can write:
-const myImg = document.querySelector("#img"); // we can not write in .tjs file
-img.src = "url"; // TS know that img is tag image that why they offer us more query on image tag
-// 1s variation:
-// let year: HTMLElement | null;
-// year = document.querySelector("year");
-// let thisYear: string;
-// thisYear = new Date().getFullYear().toString();
-// if (year) {
-//   year.setAttribute("datetime", thisYear);
-//   year.textContent = thisYear;
-// }
-// 2nd variation
-let year = document.querySelector("year");
-let thisYear = new Date().getFullYear().toString();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
+    play(action) {
+        return "";
+    }
+}
+class Peeps {
+    constructor(other = Peeps.getCount()) {
+        this.other = other;
+    }
+    static getCount() {
+        return Peeps.count + 50;
+    }
+    getOther() {
+        console.log(this.other);
+    }
+}
+Peeps.count = 1;
+const newPeeps = new Peeps();
+// console.log(newPeeps.getCount());
+newPeeps.getOther();
+class other extends Peeps {
+}
+const a = new other();
+// console.log(a.getCount());
--- dave/01-intro/src/main.ts
+++ dave/01-intro/src/main.ts
@@ -1,48 +1,29 @@
-type One = string;
-type Two = string | number;
-type Three = "hello";
-let a: One = "a";
-let b = a as Two; // less specific
-let c = a as Three; // more specific
-let d = <One>"world";
-let e = <string | number>"world";
-const addOrConcat = (a: number, b: number, c: "add" | "concat"): number | string => {
-  if (c === "add") {
-    return a + b;
+interface Musician {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Musician {
+  constructor(public name: string, public instrument: string) {}
+  play(action: string): string {
+    return "";
   }
-  return "" + a + b;
-};
-// we know that addOrConcat will return as string .
-// ==> so we use  'as string' to specify type of fn.
-// ==> so we will assume that fn will return only string event it can be number|string.
+}
+class Peeps {
+  static count: number = 1;
+  constructor(public other: number = Peeps.getCount()) {}
+  static getCount(): number {
+    return Peeps.count + 50;
+  }
+  getOther() {
+    console.log(this.other);
+  }
+}
+const newPeeps = new Peeps();
+// console.log(newPeeps.getCount());
+newPeeps.getOther();
+class other extends Peeps {}
 
-let myVal: string = addOrConcat(2, 2, "concat") as string;
-//1. be careful that fn may return a string:
-let nexVal: number = addOrConcat(2, 2, "concat") as number;
-
-console.log(myVal);
-
-//2. the DOM :
-const img = document.querySelector("img") as HTMLImageElement;
-//  but we can write:
-const myImg = <HTMLImageElement>document.querySelector("#img"); // we can not write in .tjs file
-
-img.src = "url"; // TS know that img is tag image that why they offer us more query on image tag
-
-// 1s variation:
-// let year: HTMLElement | null;
-// year = document.querySelector("year");
-// let thisYear: string;
-// thisYear = new Date().getFullYear().toString();
-// if (year) {
-//   year.setAttribute("datetime", thisYear);
-//   year.textContent = thisYear;
-// }
-
-// 2nd variation
-
-let year = document.querySelector("year") as HTMLElement;
-
-let thisYear: string = new Date().getFullYear().toString();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
+const a = new other();
+// console.log(a.getCount());
