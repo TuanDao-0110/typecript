adbcde684722f796dacbfd41445178fb9632cd2d
tuan <rickytuan1991@gmail.com>
2022-12-29 15:52:54 +0200

    add

--- dave/01-intro/build/js/main.js
+++ dave/01-intro/build/js/main.js
@@ -11,3 +11,26 @@ let multiply = (a, b) => {
     return 4;
 };
 console.log(multiply(4, 4));
+const addAll = (a = 10, b, c = 4) => {
+    return a + b + c;
+};
+console.log(addAll(undefined, 3));
+// Rest Parameter:
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => {
+        return prev + curr;
+    });
+};
+console.log(total(2, 4, 5, 10));
+// never fn
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const numberOrString = (val) => {
+    if (typeof val === "string")
+        return "val";
+    //   return createError("this should be string");
+    // not like this
+    return createError("use string");
+};
+console.log(numberOrString(4));
--- dave/01-intro/src/main.ts
+++ dave/01-intro/src/main.ts
@@ -20,14 +20,43 @@ const add = (a: number, b: number): number => {
   return b + a;
 };
 
-// type vs interface fn 
+// type vs interface fn
 interface mathFn {
   (a: number, b: number): number;
 }
-
 
 let multiply: mathFn = (a, b) => {
   return 4;
 };
 
 console.log(multiply(4, 4));
+
+const addAll = (a: number = 10, b: number, c = 4): number => {
+  return a + b + c;
+};
+
+console.log(addAll(undefined, 3));
+
+// Rest Parameter:
+
+const total = (...nums: number[]): number => {
+  return nums.reduce((prev, curr) => {
+    return prev + curr;
+  });
+};
+
+console.log(total(2, 4, 5, 10));
+
+// never fn
+const createError = (errMsg: string): never => {
+  throw new Error(errMsg);
+};
+
+const numberOrString = (val: number | string): string => {
+  if (typeof val === "string") return "val";
+  //   return createError("this should be string");
+  // not like this
+  return createError("use string");
+};
+
+console.log(numberOrString(4));
